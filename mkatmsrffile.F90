2633cd01016dadfab5a397b6f79721b4ab788d14
Balwinder Singh <balwindersingh@gmail.com>
2019-06-24 17:36:31 -0700

    more cleanup

--- mkatmsrffile.F90
+++ mkatmsrffile.F90
@@ -1,196 +1,169 @@
 !===============================================================================
-! SVN $Id:  $
-! SVN $URL: $
 ! 12/06/2010  Jim Edwards jedwards@ucar.edu
+! 06/24/2018  Balwinder Singh (balwinder.singh@pnnl.gov) removed pio, mct, mpi 
+!             and csm_share dependencies
+!
 ! Interpolate files needed for cam atmosphere dry deposition to model grid
 !===============================================================================
 
 program mkatmsrffile
-  use netcdf 
-
+  use netcdf, only: nf90_open, nf90_inq_dimid, nf90_inquire_dimension,      &
+       nf90_inq_varid, nf90_get_var, nf90_close, NF90_NOWRITE, nf90_create, &
+       nf90_def_dim, nf90_def_var, nf90_enddef, nf90_put_var, NF90_NOERR,   &
+       nf90_strerror, NF90_CLOBBER, NF90_DOUBLE
 
   implicit none
 
-  !remove dependence on csm share
+  !Define datatypes
   integer,parameter :: r8 = selected_real_kind(12) ! 8 byte real
   integer,parameter :: shr_kind_cl = 256           ! long char
   integer,parameter :: shr_kind_cx = 512           ! extra-long char
 
-  integer :: ierr, npes, iam, npft
-  integer, pointer :: sfcgindex(:), atmgindex(:)
+  !Other parameters
+  real(r8),parameter :: huge_real = huge(1.0_r8)
 
+  
+  !Variables
   type rptr
      real(r8), pointer :: fld(:)
   end type rptr
 
   type(rptr), pointer :: soilw(:), pft(:), apft(:), asoilw(:)
 
-  integer, pointer :: comps(:) ! array with component ids
-  integer, pointer :: comms(:) ! array with mpicoms
+  character(len=shr_kind_cl) :: srffilename, atmfilename
+  character(len=shr_kind_cl) :: soilwfilename, landfilename, outputfilename
 
-  character(len=shr_kind_cl) :: srffilename
-  character(len=shr_kind_cl) :: atmfilename
+  character(len=shr_kind_cx) :: srf2atmFmapname
 
-  character(len=shr_kind_cl) :: soilwfilename
-  character(len=shr_kind_cl) :: landfilename
+  integer :: srfnx, atmnx, dimid, nlat, nlon, i, j, dim1, dim2, npft
+  integer :: ncid_map, ncid_land, ncid_soil, ncid_out
+  integer :: varid, varid1,varid2,n_a,n_b,n_s, total_grd_pts
 
-  character(len=shr_kind_cl) :: outputfilename
-  character(len=shr_kind_cx) :: mapname,srf2atmFmapname
-  character(len=shr_kind_cl) :: maptype,srf2atmFmaptype
-  
+  integer,pointer :: col(:),row(:)
 
-  integer, pointer :: dof(:), dof2(:), dof3(:)
+  real(r8) :: total_land, fraction_soilw  
+
   real(r8), pointer :: landmask(:),lake(:), wetland(:), urban(:)
   real(r8), pointer :: alake(:), awetland(:), aurban(:), fraction_landuse(:,:)
-  integer :: srfnx, atmnx, srfnxg, atmnxg, dimid, nlat, nlon, i, j, clen, index, dim1, dim2
-  
-  character(len=*), parameter :: srffields(5) =(/"PCT_LAKE   ",&
-                                                 "PCT_WETLAND",&
-                                                 "PCT_URBAN  ", &
-                                                 "SOILW      ", & 
-                                                 "PCT_PFT    "/)
-  
-  character(len=220) :: rList
-  character(len=6) :: str
-  real(r8) :: total_land, fraction_soilw
   real(r8), pointer :: total_soilw(:,:)
-  Character(len=*), parameter :: ConfigFileName="mkatmsrffile.rc"
+  real(r8), pointer :: tmp2d(:,:), tmp3d(:,:,:)
+  real(r8), pointer :: wgt(:)
 
-  integer :: ncid,varid, ieq, varid1,varid2,n_a,n_b,n_s
-  integer,pointer :: col(:),row(:)
-  real(r8), pointer :: lake_balli(:,:),wetland_balli(:,:),urban_balli(:,:),landmask_balli(:,:),pft_balli(:,:,:),soilw_balli(:,:,:)
-  real(r8), pointer :: area_a(:),area_b(:),s(:)
 
-  
-  namelist /input/srfFileName,atmFileName,landFileName,soilwFileName,srf2atmFmapname,srf2atmFmaptype,outputFileName 
+  !Read namelist
+  namelist /input/srfFileName,atmFileName,landFileName,soilwFileName,srf2atmFmapname,outputFileName 
   
   open(1, file = 'nml_atmsrf', status = 'old')
   read(1,nml=input)
   close(1)
 
-  mapname = trim(adjustl(srf2atmFmapname))
-  maptype = trim(adjustl(srf2atmFmaptype))
-
-  ! gsmap_srf, srfnx, srfnxg are the intent-outs (see more detail in the subroutine)
+  !Read grid sizes of srf and atm files
   call openfile_and_initdecomp(srffilename, srfnx)
-  ! gsmap_atm, atmnx, atmnxg are the intent-outs ()
   call openfile_and_initdecomp(atmfilename, atmnx)
 
- 
-  ! The following routine is reading values for labels ("srf2atmFmapname:" and  "srf2atmFmaptype:") from "mkatmsrffile.rc" file
+  !Read map file for weights and other parameters required for remapping
+  call nc_check(nf90_open(trim(srf2atmFmapname), NF90_NOWRITE, ncid_map))
+  call nc_check( nf90_inq_dimid(ncid_map,"n_a", dimid) )
+  call nc_check( nf90_inquire_dimension(ncid_map, dimid, len = n_a) )
+  call nc_check( nf90_inq_dimid(ncid_map,"n_b", dimid) )
+  call nc_check( nf90_inquire_dimension(ncid_map, dimid, len = n_b) )
+  call nc_check( nf90_inq_dimid(ncid_map,"n_s", dimid) )
+  call nc_check( nf90_inquire_dimension(ncid_map, dimid, len = n_s) )
 
-      
-  ! Frm my reading of the following call:
-  !sMatP read area_a(n_a), area_b(n_b),col(n_s),row(n_s) and S(n_s) 
-  ! from mapname file. MAY BE gsmap_srf is a source grid here and 
-  ! gsmap_atm is destination
-  !sMatP has fields which stores above variables...it just stores them
-
-
-  call check(nf90_open(trim(mapname), NF90_NOWRITE, ncid))
-  call check( nf90_inq_dimid(ncid,"n_a", dimid) )
-  call check( nf90_inquire_dimension(ncid, dimid, len = n_a) )
-  call check( nf90_inq_dimid(ncid,"n_b", dimid) )
-  call check( nf90_inquire_dimension(ncid, dimid, len = n_b) )
-  call check( nf90_inq_dimid(ncid,"n_s", dimid) )
-  call check( nf90_inquire_dimension(ncid, dimid, len = n_s) )
-
-  !allocate and read vars
-  allocate(area_a(n_a))
-  call check(nf90_inq_varid(ncid,'area_a',varid))
-  call check(nf90_get_var(ncid,varid,area_a))
-
-  allocate(area_b(n_b))
-  call check(nf90_inq_varid(ncid,'area_b',varid))
-  call check(nf90_get_var(ncid,varid,area_b))
-
+  !allocate and read map file variables
   allocate(col(n_s))
-  call check(nf90_inq_varid(ncid,'col',varid))
-  call check(nf90_get_var(ncid,varid,col))
+  call nc_check(nf90_inq_varid(ncid_map,'col',varid))
+  call nc_check(nf90_get_var(ncid_map,varid,col))
 
   allocate(row(n_s))
-  call check(nf90_inq_varid(ncid,'row',varid))
-  call check(nf90_get_var(ncid,varid,row))
+  call nc_check(nf90_inq_varid(ncid_map,'row',varid))
+  call nc_check(nf90_get_var(ncid_map,varid,row))
 
-  allocate(s(n_s))
-  call check(nf90_inq_varid(ncid,'S',varid))
-  call check(nf90_get_var(ncid,varid,s))
+  allocate(wgt(n_s))
+  call nc_check(nf90_inq_varid(ncid_map,'S',varid))
+  call nc_check(nf90_get_var(ncid_map,varid,wgt))
 
-  call check(nf90_close ( ncid ))
+  !Close map file
+  call nc_check(nf90_close ( ncid_map ))
 
-
-
-
-  call check(nf90_open(landfilename, NF90_NOWRITE, ncid))
+  !Read Land file
+  call nc_check(nf90_open(landfilename, NF90_NOWRITE, ncid_land))
   
-  call check( nf90_inq_dimid(ncid, "lon", dimid) )
-  call check( nf90_inquire_dimension(ncid, dimid, len = nlon) )
-  call check( nf90_inq_dimid(ncid, "lat", dimid) )
-  call check( nf90_inquire_dimension(ncid, dimid, len = nlat) )
-  call check( nf90_inq_dimid(ncid, "pft", dimid) )
-  call check( nf90_inquire_dimension(ncid, dimid, len = npft) )
+  call nc_check( nf90_inq_dimid(ncid_land, "lon", dimid) )
+  call nc_check( nf90_inquire_dimension(ncid_land, dimid, len = nlon) )
+  call nc_check( nf90_inq_dimid(ncid_land, "lat", dimid) )
+  call nc_check( nf90_inquire_dimension(ncid_land, dimid, len = nlat) )
 
+  total_grd_pts = nlon*nlat
 
-  allocate(lake_balli(nlon,nlat),lake(nlon*nlat))
-  call check(nf90_inq_varid(ncid,'PCT_LAKE',varid))
-  call check(nf90_get_var(ncid,varid,lake_balli))
-  lake(:) = reshape(lake_balli,(/nlon*nlat/))
-  
+  call nc_check( nf90_inq_dimid(ncid_land, "pft", dimid) )
+  call nc_check( nf90_inquire_dimension(ncid_land, dimid, len = npft) )
+
+  !Allocate temporary variables to read data from netcdf file
+  allocate(tmp2d(nlon,nlat),tmp3d(nlon,nlat,npft))
+  tmp2d(:,:)   = huge_real
+  tmp3d(:,:,:) = huge_real
+
+  allocate(lake(total_grd_pts))
+  call nc_check(nf90_inq_varid(ncid_land,'PCT_LAKE',varid))
+  call nc_check(nf90_get_var(ncid_land,varid,tmp2d))
+  lake(:) = reshape(tmp2d,(/total_grd_pts/))
   lake = lake * 0.01_r8
 
-  allocate(pft_balli(nlon,nlat,npft))
-  call check(nf90_inq_varid(ncid,'PCT_PFT',varid))
-  call check(nf90_get_var(ncid,varid,pft_balli))
-  print*,'spahe(pft_balli):',shape(pft_balli)
+
+  call nc_check(nf90_inq_varid(ncid_land,'PCT_PFT',varid))
+  call nc_check(nf90_get_var(ncid_land,varid,tmp3d))
+
   allocate(pft(npft),apft(npft))
 
-  !following is reading PCT_PFT array which is dimensioned (npft,nlat,nlon)
-  !following loop stores each 2d array (nlat,nlon) in pft(i)%fld, where i goes from
-  !1 to npft
+  !deallocate all memory at the end???
+  !Storing in a data structure
   do i=1,npft
-     allocate(pft(i)%fld(nlon*nlat))
-     pft(i)%fld = reshape(pft_balli(:,:,i),(/nlon*nlat/)) * 0.01_r8
+     allocate(pft(i)%fld(total_grd_pts))
+     pft(i)%fld = reshape(tmp3d(:,:,i),(/total_grd_pts/)) * 0.01_r8
   end do
 
 
-  allocate(wetland_balli(nlon,nlat),wetland(nlon*nlat))
-  call check(nf90_inq_varid(ncid,'PCT_WETLAND',varid))
-  call check(nf90_get_var(ncid,varid,wetland_balli))
-  wetland = reshape(wetland_balli,(/nlon*nlat/))
+  tmp2d(:,:)   = huge_real  !Reinitialize tmp2d to inf
+  allocate(wetland(total_grd_pts))
+  call nc_check(nf90_inq_varid(ncid_land,'PCT_WETLAND',varid))
+  call nc_check(nf90_get_var(ncid_land,varid,tmp2d))
+  wetland = reshape(tmp2d,(/total_grd_pts/))
   wetland = wetland * 0.01_r8
 
+  tmp2d(:,:)   = huge_real  !Reinitialize tmp2d to inf
+  allocate(urban(total_grd_pts))
+  call nc_check(nf90_inq_varid(ncid_land,'PCT_URBAN',varid))
+  call nc_check(nf90_get_var(ncid_land,varid,tmp2d))
 
-  allocate(urban_balli(nlon,nlat),urban(nlon*nlat))
-  call check(nf90_inq_varid(ncid,'PCT_URBAN',varid))
-  call check(nf90_get_var(ncid,varid,urban_balli))
-
-  urban = reshape(urban_balli,(/nlon*nlat/))
+  urban = reshape(tmp2d,(/total_grd_pts/))
   urban = urban * 0.01_r8
 
-  
+  tmp2d(:,:)   = huge_real    !Reinitialize tmp2d to inf
   allocate(landmask(srfnx))
-  allocate(landmask_balli(nlon,nlat))
-  call check(nf90_inq_varid(ncid,'LANDMASK',varid))
-  call check(nf90_get_var(ncid,varid,landmask_balli))
-  landmask = reshape(landmask_balli,(/nlon*nlat/))
+  call nc_check(nf90_inq_varid(ncid_land,'LANDMASK',varid))
+  call nc_check(nf90_get_var(ncid_land,varid,tmp2d))
+  landmask = reshape(tmp2d,(/total_grd_pts/))
 
 
-  call check(nf90_close ( ncid ))
+  call nc_check(nf90_close ( ncid_land ))
 
   !open soil file
-  call check(nf90_open(soilwfilename, NF90_NOWRITE, ncid))!use a different ncid variable?
+  call nc_check(nf90_open(soilwfilename, NF90_NOWRITE, ncid_soil))!use a different ncid variable?
 
-  allocate(soilw_balli(nlon,nlat,12))!make 12 a variable or read from file??
-  call check(nf90_inq_varid(ncid,'SOILW',varid))
-  call check(nf90_get_var(ncid,varid,soilw_balli))
-  print*,'shape(soilw_balli):',shape(soilw_balli)
+  deallocate(tmp3d)
+  allocate(tmp3d(nlon,nlat,12))!make 12 a variable or read from file??
+  call nc_check(nf90_inq_varid(ncid_soil,'SOILW',varid))
+  call nc_check(nf90_get_var(ncid_soil,varid,tmp3d))
+  call nc_check(nf90_close ( ncid_soil ))
 
 
   allocate(soilw(12),asoilw(12))
   do i=1,12
      allocate(soilw(i)%fld(nlat*nlon))
-     soilw(i)%fld = reshape(soilw_balli(:,:,i),(/nlat*nlon/))
+     soilw(i)%fld = reshape(tmp3d(:,:,i),(/total_grd_pts/))
   end do
+  tmp3d(:,:,:)   = huge_real   !Reinitialize tmp3d to inf
 
   do i=1,srfnx
      if(nint(landmask(i)) == 0) then
@@ -205,30 +178,22 @@ program mkatmsrffile
   deallocate(landmask)
 
 
-  !sanity checks for n_s being equal to atmnx etc.?
-  allocate(alake(n_b))
-  alake(:) = 0.0_r8
-  do i = 1, n_s !change i to longer var name with mult characters?
-     alake(row(i)) =  alake(row(i)) + s(i)*lake(col(i))
-  enddo
-  
-  allocate(awetland(n_b))
+  !sanity nc_checks for n_s being equal to atmnx etc.?
+  allocate(alake(n_b), awetland(n_b), aurban(n_b))
+  alake(:)    = 0.0_r8
   awetland(:) = 0.0_r8
+  aurban(:)   = 0.0_r8 !why can't it be huge_real???
   do i = 1, n_s !change i to longer var name with mult characters?
-     awetland(row(i)) =  awetland(row(i)) + s(i)*wetland(col(i))
+     alake(row(i)) =  alake(row(i)) + wgt(i)*lake(col(i))
+     awetland(row(i)) =  awetland(row(i)) + wgt(i)*wetland(col(i))
+     aurban(row(i)) =  aurban(row(i)) + wgt(i)*urban(col(i))
   enddo
   
-  allocate(aurban(n_b))
-  aurban(:) = 0.0_r8
-  do i = 1, n_s !change i to longer var name with mult characters?
-     aurban(row(i)) =  aurban(row(i)) + s(i)*urban(col(i))
-  enddo
-
   do j = 1, npft
      allocate(apft(j)%fld(n_b))
      apft(j)%fld(:) = 0.0_r8
      do i = 1, n_s !change i to longer var name with mult characters?
-        apft(j)%fld(row(i)) = apft(j)%fld(row(i)) + s(i)*pft(j)%fld(col(i))
+        apft(j)%fld(row(i)) = apft(j)%fld(row(i)) + wgt(i)*pft(j)%fld(col(i))
      enddo
   enddo
 
@@ -236,7 +201,7 @@ program mkatmsrffile
      allocate(asoilw(j)%fld(n_b))
      asoilw(j)%fld(:) = 0.0_r8
      do i = 1, n_s !change i to longer var name with mult characters?
-        asoilw(j)%fld(row(i)) = asoilw(j)%fld(row(i)) + s(i)*soilw(j)%fld(col(i))
+        asoilw(j)%fld(row(i)) = asoilw(j)%fld(row(i)) + wgt(i)*soilw(j)%fld(col(i))
      enddo
   enddo
 
@@ -279,20 +244,20 @@ program mkatmsrffile
 
   end do
 
-  call check( nf90_create(trim(outputfilename), NF90_CLOBBER, ncid) ) ! NEW NCID VAR?
+  call nc_check( nf90_create(trim(outputfilename), NF90_CLOBBER, ncid_out) ) ! NEW NCID VAR?
 
-  call check( nf90_def_dim(ncid, 'ncol', atmnx, dim1) )
-  call check( nf90_def_dim(ncid, 'class', 11, dim2) )
-  call check( nf90_def_var(ncid, 'fraction_landuse', NF90_DOUBLE, (/dim1,dim2/), varid1) )
-  call check( nf90_def_dim(ncid,'month',12, dim2))
-  call check( nf90_def_var(ncid,'soilw', NF90_DOUBLE, (/dim1,dim2/), varid2))
+  call nc_check( nf90_def_dim(ncid_out, 'ncol', atmnx, dim1) )
+  call nc_check( nf90_def_dim(ncid_out, 'class', 11, dim2) )
+  call nc_check( nf90_def_var(ncid_out, 'fraction_landuse', NF90_DOUBLE, (/dim1,dim2/), varid1) )
+  call nc_check( nf90_def_dim(ncid_out,'month',12, dim2))
+  call nc_check( nf90_def_var(ncid_out,'soilw', NF90_DOUBLE, (/dim1,dim2/), varid2))
 
-  call check( nf90_enddef(ncid) )
+  call nc_check( nf90_enddef(ncid_out) )
 
-  call check(nf90_put_var(ncid, varid1, fraction_landuse))
-  call check(nf90_put_var(ncid, varid2, total_soilw ))
+  call nc_check(nf90_put_var(ncid_out, varid1, fraction_landuse))
+  call nc_check(nf90_put_var(ncid_out, varid2, total_soilw ))
 
-  call check( nf90_close(ncid) )
+  call nc_check( nf90_close(ncid_out) )
 
 
 contains
@@ -301,24 +266,24 @@ contains
     character(len=*), intent(in) :: filename
     integer, intent(out) :: nx
 
-    call check(nf90_open(filename, NF90_NOWRITE, ncid))
+    call nc_check(nf90_open(filename, NF90_NOWRITE, ncid_out))
 
-    call check( nf90_inq_dimid(ncid, "grid_size", dimid) )
-    call check( nf90_inquire_dimension(ncid, dimid, len = nx) )
-    call check(nf90_close ( ncid ))
+    call nc_check( nf90_inq_dimid(ncid_out, "grid_size", dimid) )
+    call nc_check( nf90_inquire_dimension(ncid_out, dimid, len = nx) )
+    call nc_check(nf90_close ( ncid_out ))
   end subroutine openfile_and_initdecomp
 
 
 
 
-  subroutine check(status)
+  subroutine nc_check(status)
     integer, intent ( in) :: status
     
     if(status /= nf90_noerr) then 
       print *, trim(nf90_strerror(status))
       stop "Stopped"
     end if
-  end subroutine check  
+  end subroutine nc_check  
 
 end program mkatmsrffile
 
